d93b7ce9b0116eef925e127ec538db05f0460b00
Maciej Chałapuk <maciej@chalapuk.pl>
2014-04-01 23:16:08 +0200

    - annoying logs

--- router.go
+++ router.go
@@ -15,36 +15,33 @@ func NewRouter() *Router {
 }
 
 func (router *Router) AddResource(path string, controller interface{}) {
-  log.Printf("adding resource %v", path)
-
   api := router.impl.PathPrefix(path).Subrouter()
-  registered := make([]string, 0, 5)
+  registered := false
 
   if lister, ok := controller.(Lister); ok {
     api.Path("/").Methods("GET").HandlerFunc(NewListerHandler(lister))
-    registered = append(registered, "Lister")
+    registered = true
   }
   if getter, ok := controller.(Getter); ok {
     api.Path("/{id:[0-9]+}").Methods("GET").HandlerFunc(NewGetterHandler(getter))
-    registered = append(registered, "Getter")
+    registered = true
   }
   if adder, ok := controller.(Adder); ok {
     api.Path("/").Methods("POST").HandlerFunc(NewAdderHandler(adder))
-    registered = append(registered, "Adder")
+    registered = true
   }
   if replacer, ok := controller.(Replacer); ok {
     api.Path("/{id:[0-9]+}").Methods("PUT").HandlerFunc(NewReplacerHandler(replacer))
-    registered = append(registered, "Replacer")
+    registered = true
   }
   if deleter, ok := controller.(Deleter); ok {
     api.Path("/{id:[0-9]+}").Methods("DELETE").HandlerFunc(NewDeleterHandler(deleter))
-    registered = append(registered, "Deleter")
+    registered = true
   }
 
-  if len(registered) == 0 {
+  if !registered {
     log.Panicf("controller %#v doesn't implement any REST interfaces", controller)
   }
-  log.Printf("registered controller %#v implementing interfaces %v", controller, registered)
 }
 
 func (router *Router) ServeHTTP(out http.ResponseWriter, in *http.Request) {
