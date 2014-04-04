2d6258e92105886bae8f21486edb29df623b7517
Maciej Chałapuk <maciej@chalapuk.pl>
2014-04-05 01:43:46 +0200

    ~ more precise comments in hello example

--- example/hello/main.go
+++ example/hello/main.go
@@ -22,8 +22,8 @@ import (
   "net/http"
 )
 
-// First thing to do, is defining controller structure, which is responsible
-// for handling calls to RESTful API of objects identified by string keys.
+// First thing to do, is to define controller structure, which will
+// be responsible for handling calls to RESTful API.
 type MyToysController struct {
   // Memory storage implemented on map is used for simplicity.
   // Normally we would want some persistence in here.
@@ -35,22 +35,24 @@ func NewMyToysController() *MyToysController {
   return controller
 }
 
-// Our controller needs at least one method for handling REST calls.
+// Our controller needs to implement at least one interface for handling
+// REST calls (check controller.go for all possible interfaces).
+// Interface restful.Indexer will be our choise for this example.
 
-// Index() method will be called when accessing root ("/") of our RESTful
-// resource using GET method (see HTTP/1.1 spec). It must return a map 
-// of type map[string]interface{}. Returned map will be encoded to json
-// and written into body of HTTP response.
+// Index() method will be called by the library on each access to root
+// of our RESTful resource ("/") that uses GET method (see HTTP/1.1 spec).
+// It must return a map of type map[string]interface{}. Returned map
+// will be encoded to JSON and written into the body of HTTP response.
 func (controller *MyToysController) Index() map[string]interface{} {
   return controller.storage
 }
 
-// Its good to check if proper interface is implemented (in this case
-// restful.Indexer) to force early crash in case of method signature error.
+// It's good to check if proper interface is implemented by the controller,
+// so we will face early crash in case of method signature error.
 var _ restful.Indexer = &MyToysController{}
 
-// Lastly, we create router, configure our RESTful resource, and start
-// HTTP server. Voilà!
+// Lastly, we need to create router, configure our RESTful resource,
+// and start HTTP server. Voilà!
 func main() {
   router := restful.NewRouter()
   router.AddResource("/api/mytoys", NewMyToysController())
