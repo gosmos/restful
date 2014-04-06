daf3f64bbb21954b844bec11135ec705fefac7ff
Maciej Chałapuk <maciej@chalapuk.pl>
2014-04-06 23:14:06 +0200

    + documentation of controller interfaces

--- controller.go
+++ controller.go
@@ -16,21 +16,82 @@
 
 package restful
 
+/*
+  Lists all objects contained in this REST resource.
+*/
 type Indexer interface {
+  /*
+    Invoked by the router when call to root of the resource ("/")
+    is received via GET HTTP method. Returned map will be JSON-encoded
+    and writen into HTTP response.
+  */
   Index() map[string]interface{}
 }
+
+/*
+  Returns object of given id contained in this REST resource.
+*/
 type Shower interface {
+  /*
+    Invoked by the router when class to resource object ("/{id}")
+    is received via GET HTTP method. Returned object will be put in
+    a response map under key of resource id. Map will be JSON-encoded
+    and written into HTTP response.
+  */
   Show(string) interface{}
 }
+
+/*
+  Adds object to this REST resource.
+*/
 type Creator interface {
+  /*
+    Invoked by the router when call to root of the resource ("/")
+    if received via POST HTTP method. Returned object will be used
+    to decode JSON-encoded body of HTTP request into. After filling
+    with data, object will be passed to Create() method.
+  */
   New() interface{}
+  /*
+    Invoked by the router after decoding body of HTTP request into
+    the object returned from New() method. Method is responsible
+    for generating id for given object and returning it. Returned
+    id will be used as a key of new object in a response map.
+    Map will be JSON-encoded and written into HTTP response.
+  */
   Create(interface{}) string
 }
+
+/*
+  Replaces content of an object contained in this REST resource.
+*/
 type Updater interface {
+  /*
+    Invoked by the router when call to resource object ("/{id}")
+    if received via PUT HTTP method. Returned object will be used
+    to decode JSON-encoded body of HTTP request into. After filling
+    with data, object will be passed to Update() method.
+  */
   New() interface{}
+  /*
+    Invoked by the router after decoding body of HTTP request into
+    the object returned from New() method. Given id will be used as
+    a key of given object in a response map. Map will be JSON-encoded
+    and written into HTTP response.
+  */
   Update(string, interface{})
 }
+
+/*
+  Deletes object from this REST resource.
+*/
 type Deleter interface {
+  /*
+    Invoked by the router when class to resource object ("/{id}")
+    is received via DELETE HTTP method. Returned boolean will be
+    put in a response map under key "ok". Map will be JSON-encoded
+    and written into HTTP response.
+  */
   Delete(string) bool
 }
 
